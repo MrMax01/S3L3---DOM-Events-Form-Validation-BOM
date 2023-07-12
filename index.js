4ad7c654225181e7b57b742dc666385025d708ee
MrMax01 <massimo.mauro00@gmail.com>
2023-07-12 15:32:14 +0200

    btn delete fixed

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
+const todoList = [];
+
+const list = () => {
+  let todoListHTML = "";
+  for (let i = 0; i < todoList.length; i++) {
+    todoListHTML += `
+        <div>
+        <p>${todoList[i]}</p>
+        <button class="js-delete-btn" onclick ="deleteTODO(${i})">Delete</button>
+        </div>`;
+  }
+  const sectionList = document.querySelector("section");
+  sectionList.innerHTML = todoListHTML;
+};
+
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
   const inputTask = event.target[0].value;
   addTODO(inputTask);
-});
-
-document.querySelector("body").addEventListener("keydown", (key) => {
-  if (key === "Enter") {
-    const inputTask = document.querySelector("#task-name").value;
-    addTODO(inputTask);
-  }
-});
-
-document.querySelectorAll(".js-delete-btn").forEach((elem, index) => {
-  elem.addEventListener("click", () => {});
-});
-
-function addTODO(task) {
-  const sectionList = document.querySelector("section");
-  todoList.push(task);
-  sectionList.innerHTML += `
-        <div>
-            <p>${task}</p>
-            <button class="js-delete-btn">Delete</button>
-        </div>`;
   document.querySelector("input").value = "";
+});
+
+/*Add the task to the array and update the list on the screen*/
+function addTODO(task) {
+  todoList.push(task);
+  list();
+}
+
+/*Remove the task to the array and update the list on the screen*/
+function deleteTODO(i) {
+  todoList.splice(i, 1);
+  list();
 }
